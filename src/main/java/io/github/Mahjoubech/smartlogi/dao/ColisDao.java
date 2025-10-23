e73cd6aec949fac63fa417851f743191814d6094
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-23 21:08:52 +0100

    Refactor ColisDao to extend JpaRepository and add method to find Colis by Livreur ID

--- src/main/java/io/github/Mahjoubech/smartlogi/dao/ColisDao.java
+++ src/main/java/io/github/Mahjoubech/smartlogi/dao/ColisDao.java
@@ -1,6 +1,10 @@
 package io.github.Mahjoubech.smartlogi.dao;
 
 import io.github.Mahjoubech.smartlogi.entity.Colis;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ColisDao extends Crud<Colis> {
+import java.util.List;
+
+public interface ColisDao extends JpaRepository<Colis,Long> {
+    List<Colis> findByLivreurId(Long livreurId);
 }
