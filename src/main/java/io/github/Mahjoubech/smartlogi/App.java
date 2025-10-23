043313119bd3b164f02795d63eaf345803a4b071
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-23 02:42:08 +0100

    Integrate Spring context loading and update database connection URL; refactor unit tests to use JUnit 5 : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMAR-2

--- src/main/java/io/github/Mahjoubech/smartlogi/App.java
+++ src/main/java/io/github/Mahjoubech/smartlogi/App.java
@@ -1,5 +1,8 @@
 package io.github.Mahjoubech.smartlogi;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+        String springConfigFile = "application-context.xml";
+    try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(springConfigFile)){
+        System.out.println("✅ Succès du chargement du Spring Context.");
+        System.out.println("Nombre de Beans définis : " + context.getBeanDefinitionCount());
+    }catch (Exception e) {
+    System.err.println("❌ Échec du chargement du Spring Context !");
+    System.err.println("Vérifiez le fichier " + springConfigFile + " et la connexion à la base de données.");
+    e.printStackTrace();
+}
+}
 }
--- src/test/java/io/github/Mahjoubech/smartlogi/AppTest.java
+++ src/test/java/io/github/Mahjoubech/smartlogi/AppTest.java
@@ -1,38 +1,17 @@
 package io.github.Mahjoubech.smartlogi;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+        @Test
+        void testApp() {
+            assertTrue(true);
+        }
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
