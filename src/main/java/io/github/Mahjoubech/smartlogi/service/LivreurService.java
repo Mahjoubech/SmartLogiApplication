6f4394b977e9be520fd6c094068d8ffc696d280e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-23 21:39:30 +0100

    Créer l’interface LivreurService : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMAR-13

--- src/main/java/io/github/Mahjoubech/smartlogi/service/LivreurService.java
+++ src/main/java/io/github/Mahjoubech/smartlogi/service/LivreurService.java
@@ -1,4 +1,15 @@
 package io.github.Mahjoubech.smartlogi.service;
 
-public class LivreurService {
+import io.github.Mahjoubech.smartlogi.dto.LivreurDto;
+import io.github.Mahjoubech.smartlogi.entity.Livreur;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LivreurService {
+    Livreur create(LivreurDto livreurDto);
+    List<Livreur> getAllLivreurs();
+    Optional<Livreur> getLivreurById(Long id);
+    Livreur update(Long id, LivreurDto livreurDto);
+    void delete(Long id);
 }
